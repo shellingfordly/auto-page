458ee356ac87683d5aa8d3d2279c99d1b74ab903
shellingfordly <liuyao@liuyaodeiMac.local>
2022-06-24 13:46:50 +0800

    chore: add auto import arco components, arco style, and composition api

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { Button as AButton } from "@arco-design/web-vue";
 </script>
 
 <template>
+  <a-button type="primary">Primary</a-button>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ArcoResolver } from "unplugin-vue-components/resolvers";
+import { createStyleImportPlugin } from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    AutoImport({}),
+    Components({
+      resolvers: [
+        ArcoResolver({
+          sideEffect: true,
+        }),
+      ],
+    }),
+    createStyleImportPlugin({
+      libs: [
+        {
+          libraryName: "@arco-design/web-vue",
+          esModule: true,
+          resolveStyle: (name) => {
+            // less
+            return `@arco-design/web-vue/es/${name}/style/index.js`;
+          },
+        },
+      ],
+    }),
+  ],
+});
